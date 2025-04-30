cb76a0aaef757ce7d15489ab4719ea5cc371b98e
Bernardo <bernardolimasilva13139@gmail.com>
2025-04-30 10:13:09 -0300

    Projeto no main

---
+++ src/vo/Aluno.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vo;
+
+/**
+ *
+ * @author Pichau
+ */
+public class Aluno {
+
+    private int id = 0;
+    private String nome;
+    private int serie;
+    private String turma;
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the serie
+     */
+    public int getSerie() {
+        return serie;
+    }
+
+    /**
+     * @param serie the serie to set
+     */
+    public void setSerie(int serie) {
+        this.serie = serie;
+    }
+
+    /**
+     * @return the turma
+     */
+    public String getTurma() {
+        return turma;
+    }
+
+    /**
+     * @param turma the turma to set
+     */
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    
+
+}
